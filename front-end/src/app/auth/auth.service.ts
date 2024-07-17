bf6e72cf66c17a9878cec1f85c7bbcf6822f250c
dingyang.chen <chen0z7dy@gmail.com>
2024-07-17 10:55:07 +0200

    signup form working 2 TODO: implement password decoder for admin purposes

---
+++ front-end/src/app/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  httpClient = inject(HttpClient);
+  baseUrl = 'http://localhost:8080/nessun_dorma'
+
+  constructor() { }
+
+  signup(data: any) {
+    return this.httpClient.post(`${this.baseUrl}/auth/signup`, data);
+  }
+
+  login(data: any) {
+    return this.httpClient.post(`${this.baseUrl}/login`, data)
+      .pipe(tap((result) => {
+        localStorage.setItem('authUser', JSON.stringify(result));
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem('authUser');
+  }
+
+  isLoggedIn() {
+    return localStorage.getItem('authUser') !== null;
+  }
+
+}
